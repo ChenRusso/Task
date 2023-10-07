ec140e153e7a4aa0e9a712aea8ea8e02e5ab6bcd
Chen <chenrusso2404@gmail.com>
2023-10-07 16:51:31 +0300

    Last

--- src/server/appServer.js
+++ src/server/appServer.js
@@ -1,148 +1,148 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const { Server } = require('socket.io');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const questionModel = require("./model/questionModel");
+  const express = require('express');
+  const app = express();
+  const http = require('http');
+  const { Server } = require('socket.io');
+  const cors = require('cors');
+  const mongoose = require('mongoose');
+  const questionModel = require("./model/questionModel");
 
-app.use(cors());
-app.use(express.json());
+  app.use(cors());
+  app.use(express.json());
 
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: '*',
-  },
-});
+  const server = http.createServer(app);
+  const io = new Server(server, {
+    cors: {
+      origin: '*',
+    },
+  });
 
-const mongoURI = "mongodb+srv://chenrussotask:LianBar1@moveohometask.rqlck7m.mongodb.net/ProgrammingQuestions"
+  const mongoURI = "mongodb+srv://chenrussotask:LianBar1@moveohometask.rqlck7m.mongodb.net/ProgrammingQuestions"
 
-let firstSocketIdConnected = null;  // Variable to store the first connected user's socket ID
+  let firstSocketIdConnected = null;  // Variable to store the first connected user's socket ID
 
-async function connectToMongoDB() {
-  console.log('Calling connectToMongoDB...');  // Add this line
-  try {
-    console.log('Connecting to MongoDB...');
-    await mongoose.connect(mongoURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+  async function connectToMongoDB() {
+    console.log('Calling connectToMongoDB...');  // Add this line
+    try {
+      console.log('Connecting to MongoDB...');
+      await mongoose.connect(mongoURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log('Connected to MongoDB');
+    } catch (error) {
+      console.error('Error connecting to MongoDB:', error);
+    }
+  }
+
+  io.on('connection', (socket) => {
+
+    console.log(`User Connected: ${socket.id}`);
+
+    if (firstSocketIdConnected === null) {
+      firstSocketIdConnected = socket.id;
+      console.log(`First user connected: ${firstSocketIdConnected}`);
+    }
+
+    socket.on('send_message', (data) => {
+      io.emit('receive_message', data);
     });
-    console.log('Connected to MongoDB');
-  } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
-  }
-}
 
-io.on('connection', (socket) => {
+    socket.on('typing', () => {
+      socket.broadcast.emit('user_typing', { id: socket.id });
+    });
 
-  console.log(`User Connected: ${socket.id}`);
+    socket.on('stop_typing', () => {
+      socket.broadcast.emit('user_stop_typing', { id: socket.id });
+    });
 
-  if (firstSocketIdConnected === null) {
-    firstSocketIdConnected = socket.id;
-    console.log(`First user connected: ${firstSocketIdConnected}`);
-  }
+    socket.on('get_all_questions', async (data) => {
+      try {
+        const questions = await getQuestions();
+        socket.emit('send_questions', questions);  // Send questions to the client who requested
+      } catch (error) {
+        console.error('Error getting questions:', error);
+      }
+    });
 
-  socket.on('send_message', (data) => {
-    io.emit('receive_message', data);
+    socket.on('is_first_user', () => {
+      console.log(socket.id)
+      console.log(firstSocketIdConnected)
+      socket.emit('receive_is_first_user', socket.id === firstSocketIdConnected);
+    });
+
+    socket.on("question_answer",async (id)=>{
+      try {
+        const questionsAnswer = await getQuestionAnswerById(id);
+        socket.emit('send_question_answer', questionsAnswer);  // Send questions to the client who requested
+      } catch (error) {
+        console.error('Error getting questions:', error);
+      }
+    })
+
+    /*socket.on('question_code_by_id', async (id) => {
+      try {
+        const questionsCodeById = await getQuestionCodeById(id);
+        socket.emit('send_question_code_by_id', questionsCodeById); // Send questions to the client who requested
+      } catch (error) {
+        console.error('Error getting questions:', error);
+      }
+    });
+
+     */
+
+    app.get('/request_question/:id', async (req, res) => {
+      const questionId = req.params.id;
+
+      try {
+        const question = await getQuestionCodeById(questionId);
+        res.json({ question });
+      } catch (error) {
+        console.error('Error getting question:', error);
+        res.status(500).json({ error: 'Error getting question' });
+      }
+    });
+
   });
 
-  socket.on('typing', () => {
-    socket.broadcast.emit('user_typing', { id: socket.id });
-  });
+  (async () => {
+    await connectToMongoDB();
+    server.listen(3001, () => {
+      console.log('Server is running on port 3001');
+    });
+  })();
 
-  socket.on('stop_typing', () => {
-    socket.broadcast.emit('user_stop_typing', { id: socket.id });
-  });
-
-  socket.on('get_all_questions', async (data) => {
+  const getQuestions = async () => {
     try {
-      const questions = await getQuestions();
-      socket.emit('send_questions', questions);  // Send questions to the client who requested
+      // Get all the questions from the database
+      const questions = await questionModel.find({});
+
+      return questions;
     } catch (error) {
-      console.error('Error getting questions:', error);
+      console.error('Error retrieving questions:', error);
     }
-  });
+  };
 
-  socket.on('is_first_user', () => {
-    console.log(socket.id)
-    console.log(firstSocketIdConnected)
-    socket.emit('receive_is_first_user', socket.id === firstSocketIdConnected);
-  });
-
-  socket.on("question_answer",async (id)=>{
+  const getQuestionCodeById = async (id) => {
     try {
-      const questionsAnswer = await getQuestionAnswerById(id);
-      socket.emit('send_question_answer', questionsAnswer);  // Send questions to the client who requested
-    } catch (error) {
-      console.error('Error getting questions:', error);
-    }
-  })
+      // Get all the questions from the database
+      const question = await questionModel.findById(id);
 
-  /*socket.on('question_code_by_id', async (id) => {
+      return question;
+    } catch (error) {
+      console.error('Error retrieving questions:', error);
+    }
+  };
+
+  const getQuestionAnswerById = async (id) => {
     try {
-      const questionsCodeById = await getQuestionCodeById(id);
-      socket.emit('send_question_code_by_id', questionsCodeById); // Send questions to the client who requested
+      // Get all the questions from the database
+      const question = await questionModel.findById(id);
+
+      return question.answer;
     } catch (error) {
-      console.error('Error getting questions:', error);
+      console.error('Error retrieving questions:', error);
     }
-  });
-
-   */
-
-  app.get('/request_question/:id', async (req, res) => {
-    const questionId = req.params.id;
-
-    try {
-      const question = await getQuestionCodeById(questionId);
-      res.json({ question });
-    } catch (error) {
-      console.error('Error getting question:', error);
-      res.status(500).json({ error: 'Error getting question' });
-    }
-  });
-
-});
-
-(async () => {
-  await connectToMongoDB();
-  server.listen(3001, () => {
-    console.log('Server is running on port 3001');
-  });
-})();
-
-const getQuestions = async () => {
-  try {
-    // Get all the questions from the database
-    const questions = await questionModel.find({});
-
-    return questions;
-  } catch (error) {
-    console.error('Error retrieving questions:', error);
-  }
-};
-
-const getQuestionCodeById = async (id) => {
-  try {
-    // Get all the questions from the database
-    const question = await questionModel.findById(id);
-
-    return question;
-  } catch (error) {
-    console.error('Error retrieving questions:', error);
-  }
-};
-
-const getQuestionAnswerById = async (id) => {
-  try {
-    // Get all the questions from the database
-    const question = await questionModel.findById(id);
-
-    return question.answer;
-  } catch (error) {
-    console.error('Error retrieving questions:', error);
-  }
-};
+  };
 
 
 
